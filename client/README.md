c1835276151550f2b26e0d3a43d9705cf3a695aa
Shivansh Goel <shivanshgoel21@gmail.com>
2021-11-23 16:36:07 +0530

    client: boilerplate application

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# Technoquiz Client Application
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      App Working
+    </div>
+  );
+}
+
+export default App;

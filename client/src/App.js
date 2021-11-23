1af312fbeed69a5901fda04955c85a61ff4e560e
Shivansh Goel <shivanshgoel21@gmail.com>
2021-11-23 16:51:30 +0530

    client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
+// Notifications
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
+
+// pages
+import Homepage from './pages/Homepage'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      App Working
+    <div className="app">
+      <ReactNotification />
+      <Router>
+        <div className="app__header">
+          <Link to="/">
+            Homepage
+          </Link>
+        </div>
+
+        <div className="app__body">
+        <Routes>
+
+            <Route exact path="/" element={<Homepage/>}/>
+            {/* <Route exact path="/login" element={<Login/>}/>
+            <Route exact path="/recovery-password" element={<RecoveryPassword/>}/> */}
+            <Route path="*" element={<NotFound/>}/>
+
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// components
+import Card from '../components/Card'
+
+function Homepage() {
+    return (
+        <div>
+            This is homepage
+            <Card />
+        </div>
+    )
+}
+
+export default Homepage

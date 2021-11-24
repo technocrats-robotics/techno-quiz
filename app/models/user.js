3ac2338e25c95a1d48f9c3c2e972a00359c0e434
hariketsheth <hariket.sukeshkumar2020@gmail.com>
2021-11-25 02:15:25 +0530

    Updated user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,29 @@
 // schema for user collection
-const mongoose = require('mongoose')
-const userType = require('../utils/user')
+const mongoose = require("mongoose");
+const userType = require("../utils/user");
 
 const user = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
-        required: true
+        required: true,
     },
     createdOn: {
         type: Date,
-        default: Date.now
+        default: () => Date.now(),
     },
     role: {
-        type: Number,
-        default: userType[201]
+        type: String,
+        default: userType[201],
     }
 });
 
-const User = mongoose.model('User', user)
-module.exports = User
+const User = mongoose.model("User", user);
+module.exports = User;

89082c8e5929506e2e8b39e1ae35f3c7b1a3db8d
LitoleNINJA <sritwik2@gmail.com>
2021-11-26 14:19:53 +0530

    modified db config

--- app/config/db.js
+++ app/config/db.js
@@ -1,5 +1,8 @@
 // database configuration
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connection = async () => {
     try {

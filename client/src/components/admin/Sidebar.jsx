1d10c906e8f3737b62bfee071ca037b5f6d23cab
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-10 01:12:28 +0530

    Added Sidebar

---
+++ client/src/components/admin/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import { RiHomeLine, RiFileCopyLine } from "react-icons/ri";
+import { FaUserCircle } from "react-icons/fa";
+import { AiOutlinePieChart } from "react-icons/ai";
+import Badge from "./Badge";
+import AvatarImage from "../assets/avatarImage.jpg"; //Sample
+function Sidebar() {
+  return (
+      <Container>
+        </Container>
+  );
+}

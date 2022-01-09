cd94d50c5c28279b61f707694d526ffdb52fd2e0
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-10 00:57:53 +0530

    Added Admin Dashboard

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
-function AdminDashboard() {
-    return (
-        <div>
-            Admin dashboard
-        </div>
-    )
+import "./static/css/AdminDashboard.css";
+import styled from "styled-components";
+import Sidebar from "./components/admin/Sidebar";
+function App() {
+  return (
+    <Container>
+      <Sidebar />
+      <MainContent />
+    </Container>
+  );
 }
 
-export default AdminDashboard
+const Container = styled.div`
+  display: flex;
+  height: 95vh;
+  background: linear-gradient(to bottom right,  #050022 0%, #050022 70%);
+  border-radius: 2rem;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction: column;
+  }
+`;
+
+export default App;

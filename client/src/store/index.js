2cab16c2babc3e742322f0564d1df8e3d4bea69a
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-11 01:27:16 +0530

    Return state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -9,7 +9,18 @@ const reducer = (state = initState, action) => {
         localStorage.setItem('JWT_PAYLOAD', action.token);
         localStorage.setItem('_ID', action._id);
 
-        
+        return {
+            ...state, 
+            user: action.user
+        }
+    } else if (action.type === 'set_user') {
+        return {
+            ...state, 
+            user: action.user
+        }
+    } else {
+        return state;
+    }
 }
 
 const store = createStore(reducer);

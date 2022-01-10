8b8ce1be8112c772e73c16993b481a679c3dd603
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-11 01:26:51 +0530

    Added reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,5 +4,14 @@ const initState = {
     user: null
 }
 
+const reducer = (state = initState, action) => {
+    if (action.type === 'login') {
+        localStorage.setItem('JWT_PAYLOAD', action.token);
+        localStorage.setItem('_ID', action._id);
+
+        
+}
+
+const store = createStore(reducer);
 
 export default store;

5f7eaf58e1d5720d5f671cc84afcf73120b344b4
LitoleNINJA <sritwik2@gmail.com>
2022-01-22 18:48:06 +0530

    made bg image svg

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -5,17 +5,17 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 
 // components
-import backgroundImg from '../assets/backgroundImg.jpg';
+import backgroundImg from '../assets/bg.svg';
 
 function Homepage() {
 
     return (
         <div style={{
             backgroundImage: 'url(' + backgroundImg + ')',
-            backgroungRepeat: 'no-repeat',
+            backgroungRepeat: 'repeat',
             backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            height: '100%',
+            backgroundAttachment: 'fixed',
+            backgroundPosition: '50% 0',
         }}>
 
             <Box sx={{
@@ -55,10 +55,10 @@ function Homepage() {
             }}>
                 <h1 style={{
                     color: 'white',
-                    fontSize: '3rem',
+                    fontSize: '9vmin',
                     marginBottom: '3rem'
                 }}>DEPARTMENTS</h1>
-                <Grid container columnSpacing={{ xs: 1, sm: 6, md: 10, lg: 6}} rowSpacing={6}>
+                <Grid container columnSpacing={{ xs: 1, sm: 6, md: 10, lg: 6 }} rowSpacing={6}>
                     <Grid item xs={12} sm={6} md={6} lg={3}>
                         <Paper elevation={12} style={{
                             backgroundColor: '#c4c4c4',
@@ -68,72 +68,81 @@ function Homepage() {
                             justifyContent: 'space-evenly',
                             alignItems: 'center',
                             borderRadius: '1rem',
-                            backgroundImage: 'linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%)',
+                            background: 'rgba(41, 42, 46, 0.6)',
+                            boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                            backdropFilter: 'blur( 6px )',
+
                         }}>
-                            <h1 style={{
-                                color: 'white',
-                                fontSize: '5vmin'
-                            }}>Mechanical</h1>
-                            <Button variant="contained" style={{backgroundColor: '#fffd1b'}}>Click Me</Button>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6} lg={3}>
-                        <Paper elevation={12} style={{
-                            backgroundColor: '#c4c4c4',
-                            height: '40vh',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'space-evenly',
-                            alignItems: 'center',
-                            borderRadius: '1rem',
-                            backgroundImage: 'linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%)',
-                        }}>
-                            <h1 style={{
-                                color: 'white',
-                                fontSize: '5vmin'
-                            }}>Electrical</h1>
-                            <Button variant="contained" style={{backgroundColor: '#fffd1b'}}>Click Me</Button>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6} lg={3}>
-                        <Paper elevation={12} style={{
-                            backgroundColor: '#c4c4c4',
-                            height: '40vh',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'space-evenly',
-                            alignItems: 'center',
-                            borderRadius: '1rem',
-                            backgroundImage: 'linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%)',
-                        }}>
-                            <h1 style={{
-                                color: 'white',
-                                fontSize: '5vmin'
-                            }}>Programming</h1>
-                            <Button variant="contained" style={{backgroundColor: '#fffd1b'}}>Click Me</Button>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6} lg={3}>
-                        <Paper elevation={12} style={{
-                            backgroundColor: '#c4c4c4',
-                            height: '40vh',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'space-evenly',
-                            alignItems: 'center',
-                            borderRadius: '1rem',
-                            backgroundImage: 'linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%)',
-                        }}>
-                            <h1 style={{
-                                color: 'white',
-                                fontSize: '5vmin'
-                            }}>General</h1>
-                            <Button variant="contained" style={{backgroundColor: '#fffd1b'}}>Click Me</Button>
-                        </Paper>
-                    </Grid>
+                        <h1 style={{
+                            color: 'white',
+                            fontSize: '5vmin'
+                        }}>Mechanical</h1>
+                        <Button variant="outlined" style={{ color: '#fffd1b', border: '2px solid #fffd1b' }}>Click Me</Button>
+                    </Paper>
                 </Grid>
+                <Grid item xs={12} sm={6} md={6} lg={3}>
+                    <Paper elevation={12} style={{
+                        backgroundColor: '#c4c4c4',
+                        height: '40vh',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-evenly',
+                        alignItems: 'center',
+                        borderRadius: '1rem',
+                        background: 'rgba(41, 42, 46, 0.6)',
+                        boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                        backdropFilter: 'blur( 6px )',
+                    }}>
+                        <h1 style={{
+                            color: 'white',
+                            fontSize: '5vmin'
+                        }}>Electrical</h1>
+                        <Button variant="outlined" style={{ color: '#fffd1b', border: '2px solid #fffd1b' }}>Click Me</Button>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} lg={3}>
+                    <Paper elevation={12} style={{
+                        backgroundColor: '#c4c4c4',
+                        height: '40vh',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-evenly',
+                        alignItems: 'center',
+                        borderRadius: '1rem',
+                        background: 'rgba(41, 42, 46, 0.6)',
+                        boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                        backdropFilter: 'blur( 6px )',
+                    }}>
+                        <h1 style={{
+                            color: 'white',
+                            fontSize: '5vmin'
+                        }}>Programming</h1>
+                        <Button variant="outlined" style={{ color: '#fffd1b', border: '2px solid #fffd1b' }}>Click Me</Button>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} lg={3}>
+                    <Paper elevation={12} style={{
+                        backgroundColor: '#c4c4c4',
+                        height: '40vh',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-evenly',
+                        alignItems: 'center',
+                        borderRadius: '1rem',
+                        background: 'rgba(41, 42, 46, 0.6)',
+                        boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                        backdropFilter: 'blur( 6px )',
+                    }}>
+                        <h1 style={{
+                            color: 'white',
+                            fontSize: '5vmin'
+                        }}>General</h1>
+                        <Button variant="outlined" style={{ color: '#fffd1b', border: '2px solid #fffd1b' }}>Click Me</Button>
+                    </Paper>
+                </Grid>
+            </Grid>
             </Box>
-        </div>
+        </div >
     );
 }
 

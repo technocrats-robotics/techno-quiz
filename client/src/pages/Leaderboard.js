2033ab5c9f1e6d86a7e8441485a60abe19cc0553
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-02-01 14:45:56 +0530

    leaderboard working

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -1,49 +1,37 @@
-import { Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import HeaderQuiz from "../components/QuizPage/HeaderQuiz";
 import Ranking from "../components/LeaderBoard/Ranking";
 
 function Leaderboard() {
-  return (
-    <Box
-      sx={{
-        background: "linear-gradient(145deg,rgba(0,9,53,1),rgba(8,0,101,1))",
-        height: '100vh',
-        color: "white",
-        
-      }}
-    >
-      <HeaderQuiz />
-      <Typography
-        variant="h3"
-        component="h3"
-        sx={{
-          textAlign: "center",
-          paddingTop: "2rem",
-        }}
-      >
-        LEADERBOARD
-      </Typography>
-
-      <Grid container justifyContent={'center'} spacing={4} sx={{
-            height:'100%',
-      }}>
-        <Grid item xs={3}>
-          <Ranking />
-        </Grid>
-        <Grid item xs={3}>
-          <Ranking />
-        </Grid>
-        <Grid item xs={3}>
-          <Ranking />
-        </Grid>
-        <Grid item xs={3}>
-          <Ranking />
-        </Grid>
-      </Grid>
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                background:
+                    "linear-gradient(145deg,rgba(0,9,53,1),rgba(8,0,101,1))",
+                height: "100vh",
+                color: "white",
+            }}
+        >
+            <HeaderQuiz />
+            <Container sx={{
+              py: "6rem"
+            }}>
+                <Typography
+                    variant="h4"
+                    component="h3"
+                    fontSize={}
+                    sx={{
+                        textAlign: "center",
+                        paddingTop: "2rem",
+                    }}
+                >
+                    LEADERBOARD
+                </Typography>
+            </Container>
+        </Box>
+    );
 }
 
 export default Leaderboard;

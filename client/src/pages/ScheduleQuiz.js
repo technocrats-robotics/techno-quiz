1a685d8ebed1e69ec0391b628cdfe52df9cee371
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-07-01 00:43:52 +0530

    Questions for `Scheduling`

--- client/src/pages/ScheduleQuiz.js
+++ client/src/pages/ScheduleQuiz.js
@@ -1,32 +1,38 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import { Box, useMediaQuery, useTheme } from "@mui/material";
 import Content from "../components/Admin/Quiz/ScheduleQuiz";
 import Header from "../components/Admin/Header";
 import SideBar from "../components/Admin/Sidebar";
+import { useGetAvailableQuestionsQuery } from "../app/services/api";
 
 function ScheduleQuiz() {
-  const [hamburger, setHamburger] = useState(false);
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
-  return (
-    <Box>
-      <Header setHamburger={setHamburger} hamburger={hamburger} />
-      <Box
-        sx={{
-          display: "flex",
-          minHeight: "100vh",
-          height: "100%",
-          paddingTop: {
-            xs: "4rem",
-            sm: "6rem",
-          },
-        }}
-      >
-        <SideBar />
-        <Content />
-      </Box>
-    </Box>
-  );
+    const { dept, quizId } = useParams();
+    console.log(dept);
+    const [hamburger, setHamburger] = useState(false);
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up("sm"));
+    const { data, isLoading, isSuccess } = useGetAvailableQuestionsQuery(dept);
+    console.log(data);
+    return (
+        <Box>
+            <Header setHamburger={setHamburger} hamburger={hamburger} />
+            <Box
+                sx={{
+                    display: "flex",
+                    minHeight: "100vh",
+                    height: "100%",
+                    paddingTop: {
+                        xs: "4rem",
+                        sm: "6rem",
+                    },
+                }}
+            >
+                <SideBar />
+                {isSuccess && !isLoading && <Content question={data} />}
+            </Box>
+        </Box>
+    );
 }
 
 export default ScheduleQuiz;
